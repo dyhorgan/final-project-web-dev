c28261fc256d23393f3403b356c4c05379329741
Dylan Horgan <dyhorgan@gmail.com>
2022-04-19 20:58:04 -0400

    search should link now

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -32,7 +32,9 @@ const NavBar = () => {
 
               <form className="form-inline my-2 my-lg-0">
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <Link to="/search">
+                  <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </Link>
               </form>
               <li className="nav-item">
                 <Link className="nav-link" to="/profile">
